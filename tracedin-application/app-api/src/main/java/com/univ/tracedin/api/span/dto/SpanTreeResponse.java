874328103b90b2041569379266c842e4a8029a15
강지원 <kangji149@naver.com>
2024-11-08 09:00:00 +0000

    refactor : spantreeresponse

--- tracedin-application/app-api/src/main/java/com/univ/tracedin/api/span/dto/SpanTreeResponse.java
+++ tracedin-application/app-api/src/main/java/com/univ/tracedin/api/span/dto/SpanTreeResponse.java
@@ -10,10 +10,10 @@ import com.univ.tracedin.domain.span.SpanTree;
 @JsonInclude(value = NON_EMPTY)
 public record SpanTreeResponse(
         SpanResponse span,
-        List<SpanTreeResponse> children,
-        boolean hasAnomaly,
-        boolean hasError,
-        SpanExceptionResponse stackTrace) {
+        Boolean hasAnomaly,
+        Boolean hasError,
+        SpanExceptionResponse stackTrace,
+        List<SpanTreeResponse> children) {
 
     public static SpanTreeResponse from(SpanTree spanTree) {
         return from(spanTree, true); // 최상위 노드 플래그 설정
@@ -22,13 +22,13 @@ public record SpanTreeResponse(
     private static SpanTreeResponse from(SpanTree spanTree, boolean isRoot) {
         return new SpanTreeResponse(
                 SpanResponse.from(spanTree.getSpan()),
-                spanTree.getChildren().stream()
-                        .map(child -> from(child, false)) // 하위 노드는 최상위가 아님
-                        .toList(),
-                spanTree.hasAnomalySpan(),
-                spanTree.hasErrorSpan(),
+                isRoot ? spanTree.hasAnomalySpan() : null,
+                isRoot ? spanTree.hasErrorSpan() : null,
                 isRoot && spanTree.hasErrorSpan() // 최상위 노드에서만 stackTrace 설정
                         ? SpanExceptionResponse.from(spanTree.findExceptionCause())
-                        : null);
+                        : null,
+                spanTree.getChildren().stream()
+                        .map(child -> from(child, false)) // 하위 노드는 최상위가 아님
+                        .toList());
     }
 }
